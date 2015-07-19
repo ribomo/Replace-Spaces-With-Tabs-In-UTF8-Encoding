6e07d95ef85d2a06157fdf9c626f1ce28f245253
Ribo Mo <ribomo42@gmail.com>
2015-07-19 22:51:44 +0800

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Replace-Spaces-With-Tabs-In-UTF8-Encoding
+Replace spaces with tab in a big text file with UTF-8 encoding.
---
+++ replaceSpaces.py
@@ -0,0 +1,30 @@
+import codecs
+import sys
+import re
+
+print('Open file')
+file_ob = codecs.open('d:\\1.txt','r','utf-8')
+file_out = codecs.open('d:\\1.txt','w','utf-8')
+fileLines = 33409493
+try:
+    print('Start')
+    i = 1
+    isDone = 0
+    while(isDone != 1):
+            line = file_ob.readline()
+            if(line != ''):
+                    if(i%50000==0):
+                        print(str(i*100/fileLines)+"%")
+                    i+=1
+
+                    line = line.rstrip()
+                    elements = re.split(r'\s{9,}',line)
+                    info = "\t".join(elements)
+                    info += "\n"
+                    file_out.write(info)
+            else:
+                    isDone = 1
+finally:
+	file_ob.close()
+	file_out.close()
+	print('Exit')
